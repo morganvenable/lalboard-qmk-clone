825a180373d9a82802a904561a0a7683843b9505
Ben Gruver <jesusfreke@jesusfreke.com>
2021-03-25 22:01:59 -0700

    Implement timer functionality for esp32

--- tmk_core/common/esp32/timer.c
+++ tmk_core/common/esp32/timer.c
@@ -1,16 +1,30 @@
 #include <stdint.h>
 
-void     timer_init(void) {}
-void     timer_clear(void) {}
+#include "timer.h"
+#include "esp_timer.h"
+
+static uint64_t reset_point = 0;
+
+void timer_init(void) {
+    timer_clear();
+}
+
+void timer_clear(void) {
+    reset_point = esp_timer_get_time();
+}
+
 uint16_t timer_read(void) {
-	return 0;
+    return (uint16_t)timer_read32();
 }
+
 uint32_t timer_read32(void) {
-	return 0;
+	return (uint32_t)((esp_timer_get_time() - reset_point) / 1000);
 }
+
 uint16_t timer_elapsed(uint16_t last) {
-	return 0;
+    return TIMER_DIFF_16(timer_read(), last);
 }
+
 uint32_t timer_elapsed32(uint32_t last) {
-	return 0;
+    return TIMER_DIFF_32(timer_read32(), last);
 }
