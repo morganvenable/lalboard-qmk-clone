b8a2501d434d461d30486aa3830f087df9b6980c
morganvenable <venable@gmail.com>
2022-11-11 12:53:05 -0800

    fixed windows key

--- keyboards/handwired/lalboard/keymaps/default/keymap.c
+++ keyboards/handwired/lalboard/keymaps/default/keymap.c
@@ -49,15 +49,15 @@ enum layer {
 __attribute__((weak)) const uint16_t PROGMEM keymaps[NUM_LAYERS][MATRIX_ROWS][MATRIX_COLS] = {
     [NORMAL] = LAYOUT(
              /*Center           North           East            South           West*/
-        /*R1*/ KC_H,            KC_G,           KC_QUOTE,       KC_M,           KC_D,
-        /*R2*/ KC_T,            KC_W,           KC_GRAVE,       KC_C,           KC_F,
-        /*R3*/ KC_N,            KC_V,           XXXXXXX,        KC_R,           KC_B,
-        /*R4*/ KC_S,            KC_Z,           KC_BSLASH,      KC_L,           KC_RIGHT_PAREN,
+        /*R1*/ KC_J,            KC_U,           KC_QUOTE,       KC_M,           KC_H,
+        /*R2*/ KC_K,            KC_I,           KC_COLON,       KC_COMMA,       KC_Y,
+        /*R3*/ KC_L,            KC_O,           KC_LGUI,         KC_DOT,      KC_N,
+        /*R4*/ KC_SCOLON,         KC_P,           KC_BSLASH,      KC_SLASH,       KC_RBRC,
 
-        /*L1*/ KC_U,            KC_Q,           KC_I,           KC_P,           KC_DOUBLE_QUOTE,
-        /*L2*/ KC_E,            KC_DOT,         KC_Y,           KC_J,           XXXXXXX,
-        /*L3*/ KC_O,            KC_COMMA,       KC_X,           KC_K,           KC_ESC,
-        /*L4*/ KC_A,            KC_SLASH,       KC_LEFT_PAREN,  KC_SCOLON,      KC_DEL,
+        /*L1*/ KC_F,            KC_R,           KC_G,           KC_V,           KC_DOUBLE_QUOTE,
+        /*L2*/ KC_D,            KC_E,           KC_T,           KC_C,           KC_GRAVE,
+        /*L3*/ KC_S,            KC_W,           KC_B,           KC_X,           KC_ESC,
+        /*L4*/ KC_A,            KC_Q,           KC_LBRC,        KC_Z,           KC_DEL,
 
         /*Down                  Inner           Upper           Outer Upper     Outer Lower*/
         /*RT*/ MO(NAS),         KC_SPACE,       TO(FUNC),       KC_BSPACE,      KC_LALT,
@@ -81,19 +81,19 @@ __attribute__((weak)) const uint16_t PROGMEM keymaps[NUM_LAYERS][MATRIX_ROWS][MA
         /*LT*/ _______,         _______,        _______,        _______,        _______
     ),
 
-    [NAS] = LAYOUT(
+[NAS] = LAYOUT(
              /*Center           North           East            South           West*/
-        /*R1*/ KC_7,            KC_AMPR,        XXXXXXX,        KC_KP_PLUS,     KC_6,
-        /*R2*/ KC_8,            KC_KP_ASTERISK, XXXXXXX,        KC_HASH,        KC_CIRCUMFLEX,
-        /*R3*/ KC_9,            KC_LBRACKET,    KC_APP,         XXXXXXX,        XXXXXXX,
-        /*R4*/ KC_0,            KC_RBRACKET,    XXXXXXX,        XXXXXXX,        KC_RIGHT_CURLY,
+        /*R1*/ KC_7,            KC_AMPR,        KC_UNDS,        KC_KP_PLUS,     KC_6,
+        /*R2*/ KC_8,            KC_KP_ASTERISK, KC_COLON,       KC_TRNS,        KC_CIRCUMFLEX,
+        /*R3*/ KC_9,            KC_LPRN,        KC_LGUI,         KC_TRNS,        KC_SCOLON,
+        /*R4*/ KC_0,            KC_RPRN,        XXXXXXX,        KC_TRNS,        KC_RCBR,
 
-        /*L1*/ KC_4,            KC_DOLLAR,      KC_5,           KC_MINUS,    XXXXXXX,
-        /*L2*/ KC_3,            KC_DOT,         XXXXXXX,        KC_PERCENT,     XXXXXXX,
-        /*L3*/ KC_2,            KC_AT,          XXXXXXX,        XXXXXXX,        KC_ESC,
-        /*L4*/ KC_1,            KC_EXCLAIM,     KC_LEFT_CURLY,  KC_EQUAL,       KC_DEL,
+        /*L1*/ KC_4,            KC_DOLLAR,      KC_5,             KC_MINUS,      KC_SLASH,
+        /*L2*/ KC_3,            KC_HASH,        KC_GT,           KC_PERCENT,    KC_LT,
+        /*L3*/ KC_2,            KC_AT,          XXXXXXX,         KC_X,          KC_ESC,
+        /*L4*/ KC_1,            KC_EXCLAIM,     KC_TILDE,        KC_EQUAL,      KC_DEL,
 
-             /*Down             Inner           Upper           Outer Upper     Outer Lower*/
+        /*Down                  Inner           Upper           Outer Upper     Outer Lower*/
         /*RT*/ MO(NAS),         KC_SPACE,       TO(FUNC),       KC_BSPACE,      KC_LALT,
         /*LT*/ KC_LSHIFT,       KC_ENTER,       KC_NORMAL_HOLD, KC_TAB,         KC_LCTRL
     ),
@@ -117,15 +117,15 @@ __attribute__((weak)) const uint16_t PROGMEM keymaps[NUM_LAYERS][MATRIX_ROWS][MA
 
     [GAME] = LAYOUT(
              /*Center           North           East            South           West*/
-        /*R1*/ KC_H,            KC_G,           KC_QUOTE,       KC_M,           KC_D,
-        /*R2*/ KC_T,            KC_W,           KC_GRAVE,       KC_C,           KC_F,
-        /*R3*/ KC_N,            KC_V,           XXXXXXX,        KC_R,           KC_B,
-        /*R4*/ KC_S,            KC_Z,           KC_BSLASH,      KC_L,           KC_RIGHT_PAREN,
+        /*R1*/ KC_J,            KC_U,           KC_QUOTE,       KC_M,           KC_H,
+        /*R2*/ KC_K,            KC_I,           KC_COLON,       KC_COMMA,       KC_Y,
+        /*R3*/ KC_L,            KC_O,           KC_LGUI,         KC_DOT,      KC_N,
+        /*R4*/ KC_SCOLON,         KC_P,           KC_BSLASH,      KC_SLASH,       KC_RBRC,
 
-        /*L1*/ KC_U,            KC_Q,           KC_I,           KC_P,           KC_R,
-        /*L2*/ KC_E,            KC_DOT,         KC_Y,           KC_J,           KC_GRAVE,
-        /*L3*/ KC_O,            KC_COMMA,       KC_X,           KC_K,           KC_ESC,
-        /*L4*/ KC_A,            KC_SLASH,       KC_V,           KC_SCOLON,      KC_DEL,
+        /*L1*/ KC_F,            KC_R,           KC_G,           KC_V,           KC_DOUBLE_QUOTE,
+        /*L2*/ KC_D,            KC_E,           KC_T,           KC_C,           KC_GRAVE,
+        /*L3*/ KC_S,            KC_W,           KC_B,           KC_X,           KC_ESC,
+        /*L4*/ KC_A,            KC_Q,           KC_LBRC,        KC_Z,           KC_DEL,
 
              /*Down             Inner           Upper           Outer Upper     Outer Lower*/
         /*RT*/ MO(NAS),         KC_SPACE,       TO(FUNC),       KC_BSPACE,      KC_LALT,
@@ -151,15 +151,15 @@ __attribute__((weak)) const uint16_t PROGMEM keymaps[NUM_LAYERS][MATRIX_ROWS][MA
 
     [GAME_MOD_1] = LAYOUT(
              /*Center           North           East            South           West*/
-        /*R1*/ KC_H,            KC_G,           KC_QUOTE,       KC_M,           KC_D,
-        /*R2*/ KC_T,            KC_W,           KC_GRAVE,       KC_C,           KC_F,
-        /*R3*/ KC_N,            KC_V,           XXXXXXX,        KC_R,           KC_B,
-        /*R4*/ KC_S,            KC_Z,           KC_BSLASH,      KC_L,           KC_RIGHT_PAREN,
+        /*R1*/ KC_J,            KC_U,           KC_QUOTE,       KC_M,           KC_H,
+        /*R2*/ KC_K,            KC_I,           KC_COLON,       KC_COMMA,       KC_Y,
+        /*R3*/ KC_L,            KC_O,           KC_RGUI,         KC_DOT,      KC_N,
+        /*R4*/ KC_SCOLON,         KC_P,           KC_BSLASH,      KC_SLASH,       KC_RBRC,
 
-        /*L1*/ KC_4,            KC_F4,          KC_5,           KC_9,           KC_0,
-        /*L2*/ KC_3,            KC_F3,          KC_F11,         KC_8,           KC_F7,
-        /*L3*/ KC_2,            KC_F2,          KC_F10,         KC_7,           KC_F6,
-        /*L4*/ KC_1,            KC_F1,          KC_F9,          KC_6,           KC_F5,
+        /*L1*/ KC_F,            KC_R,           KC_G,           KC_V,           KC_DOUBLE_QUOTE,
+        /*L2*/ KC_D,            KC_E,           KC_T,           KC_C,           KC_GRAVE,
+        /*L3*/ KC_S,            KC_W,           KC_B,           KC_X,           KC_ESC,
+        /*L4*/ KC_A,            KC_Q,           KC_LBRC,        KC_Z,           KC_DEL,
 
              /*Down             Inner           Upper           Outer Upper     Outer Lower*/
         /*RT*/ MO(NAS),         KC_SPACE,       TO(FUNC),       KC_BSPACE,      KC_LALT,
@@ -168,15 +168,15 @@ __attribute__((weak)) const uint16_t PROGMEM keymaps[NUM_LAYERS][MATRIX_ROWS][MA
 
     [GAME_MOD_2] = LAYOUT(
              /*Center           North           East            South           West*/
-        /*R1*/ KC_H,            KC_G,           KC_QUOTE,       KC_M,           KC_D,
-        /*R2*/ KC_T,            KC_W,           KC_GRAVE,       KC_C,           KC_F,
-        /*R3*/ KC_N,            KC_V,           XXXXXXX,        KC_R,           KC_B,
-        /*R4*/ KC_S,            KC_Z,           KC_BSLASH,      KC_L,           KC_RIGHT_PAREN,
+        /*R1*/ KC_J,            KC_U,           KC_QUOTE,       KC_M,           KC_H,
+        /*R2*/ KC_K,            KC_I,           KC_COLON,       KC_COMMA,       KC_Y,
+        /*R3*/ KC_L,            KC_O,           KC_LGUI,         KC_DOT,      KC_N,
+        /*R4*/ KC_SCOLON,         KC_P,           KC_BSLASH,      KC_SLASH,       KC_RBRC,
 
-        /*L1*/ KC_F8,            KC_F4,          KC_5,           KC_9,           KC_F8,
-        /*L2*/ KC_F7,            KC_F3,          KC_F11,         KC_8,           KC_F7,
-        /*L3*/ KC_F6,            KC_F2,          KC_F10,         KC_7,           KC_F6,
-        /*L4*/ KC_F5,            KC_F1,          KC_F9,          KC_6,           KC_F5,
+        /*L1*/ KC_F,            KC_R,           KC_G,           KC_V,           KC_DOUBLE_QUOTE,
+        /*L2*/ KC_D,            KC_E,           KC_T,           KC_C,           KC_GRAVE,
+        /*L3*/ KC_S,            KC_W,           KC_B,           KC_X,           KC_ESC,
+        /*L4*/ KC_A,            KC_Q,           KC_LBRC,        KC_Z,           KC_DEL,
 
              /*Down             Inner           Upper           Outer Upper     Outer Lower*/
         /*RT*/ MO(NAS),         KC_SPACE,       TO(FUNC),       KC_BSPACE,      KC_LALT,
@@ -208,4 +208,4 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record) {
     default:
       return true;
   }
-}
+};
