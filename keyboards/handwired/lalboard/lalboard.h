a898e0978e984cf438739692fc3a4b9f7fe423ed
morganvenable <venable@gmail.com>
2022-12-22 10:42:40 -0800

    layout for svalboard v0

--- keyboards/handwired/lalboard/lalboard.h
+++ keyboards/handwired/lalboard/lalboard.h
@@ -22,7 +22,35 @@
 #pragma once
 
 #include "quantum.h"
+// #define LAYOUT( \
+//     r1d, r1n, r1e, r1s, r1w, \
+//     r2d, r2n, r2e, r2s, r2w, \
+//     r3d, r3n, r3e, r3s, r3w, \
+//     r4d, r4n, r4e, r4s, r4w, \
+//     l1d, l1n, l1e, l1s, l1w, \
+//     l2d, l2n, l2e, l2s, l2w, \
+//     l3d, l3n, l3e, l3s, l3w, \
+//     l4d, l4n, l4e, l4s, l4w, \
+//     rtd, rti, rtu, rtuo, rtlo, \
+//     ltd, lti, ltu, ltuo, ltlo) \
+// { \
+//     {r1e, r1n, r1d, r1s, r1w}, \
+//     {r2e, r2n, r2d, r2s, r2w}, \
+//     {r3e, r3n, r3d, r3s, r3w}, \
+//     {r4e, r4n, r4d, r4s, r4w}, \
+//     {rtlo,rtuo,rtd, rti, rtu}, \
+//     {l1e, l1n, l1d, l1s, l1w}, \
+//     {l2e, l2n, l2d, l2s, l2w}, \
+//     {l3e, l3n, l3d, l3s, l3w}, \
+//     {l4e, l4n, l4d, l4s, l4w}, \
+//     {ltlo,ltuo,ltd, lti, ltu} \
+// }
 
+//Layout for svalboard v0
+//1 2 3 4 5
+//S E C N W
+//Both Thumbs (same as lalboard_v2)
+//OL OU D IL MODE DOUBLE
 #define LAYOUT( \
     r1d, r1n, r1e, r1s, r1w, \
     r2d, r2n, r2e, r2s, r2w, \
@@ -35,15 +63,15 @@
     rtd, rti, rtu, rtuo, rtlo, \
     ltd, lti, ltu, ltuo, ltlo) \
 { \
-    {r1e, r1n, r1d, r1s, r1w}, \
-    {r2e, r2n, r2d, r2s, r2w}, \
-    {r3e, r3n, r3d, r3s, r3w}, \
-    {r4e, r4n, r4d, r4s, r4w}, \
+    {r1s, r1e, r1d, r1n, r1w}, \
+    {r2s, r2e, r2d, r2n, r2w}, \
+    {r3s, r3e, r3d, r3n, r3w}, \
+    {r4s, r4e, r4d, r4n, r4w}, \
     {rtlo,rtuo,rtd, rti, rtu}, \
-    {l1e, l1n, l1d, l1s, l1w}, \
-    {l2e, l2n, l2d, l2s, l2w}, \
-    {l3e, l3n, l3d, l3s, l3w}, \
-    {l4e, l4n, l4d, l4s, l4w}, \
+    {l1s, l1e, l1d, l1n, l1w}, \
+    {l2s, l2e, l2d, l2n, l2w}, \
+    {l3s, l3e, l3d, l3n, l3w}, \
+    {l4s, l4e, l4d, l4n, l4w}, \
     {ltlo,ltuo,ltd, lti, ltu} \
 }
 
